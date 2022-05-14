6ffda75736cbd0c3803132aa1b0c4c54859bbb1a
AdamSanch <adamsan1234@gmail.com>
2022-05-14 17:58:07 -0400

    Initial commit

---
+++ Assets/Scripts/snake.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class snake : MonoBehaviour
+{
+    private Vector2 _direction = Vector2.right;
+    private List<Transform> _segments;
+    public Transform segmentPrefab;
+    public int initialSize = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _segments = new List<Transform>();
+        _segments.Add(this.transform);
+        for (int i =1;i<this.initialSize;i++){
+          _segments.Add(Instantiate(this.segmentPrefab)); 
+        } 
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.W)){
+           _direction = Vector2.up;
+       }
+       else if (Input.GetKeyDown(KeyCode.A)){
+           _direction = Vector2.left;
+       }
+       else if (Input.GetKeyDown(KeyCode.S)){
+           _direction = Vector2.down;
+       }
+       else if (Input.GetKeyDown(KeyCode.D)){
+           _direction = Vector2.right;
+       }
+    }
+
+    private void FixedUpdate(){
+        for (int i = _segments.Count-1; i>0; i--){
+            _segments[i].position = _segments[i-1].position;
+
+        }
+
+        this.transform.position = new Vector3(
+            Mathf.Round(this.transform.position.x) + _direction.x,
+            Mathf.Round(this.transform.position.y) + _direction.y,
+            0.0f
+        );
+    }
+
+    private void ResetState(){
+        for (int i =1; i<_segments.Count;i++){
+            Destroy(_segments[i].gameObject);
+        }
+
+        _segments.Clear();
+        _segments.Add(this.transform);
+
+        for (int i =1;i<this.initialSize;i++){
+          _segments.Add(Instantiate(this.segmentPrefab)); 
+        }
+
+        this.transform.position = Vector3.zero;
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    private void Grow(){
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Food"){
+            Grow();
+        }
+        else if (other.tag == "Obsticle"){
+            ResetState();
+        }
+    }
+}
